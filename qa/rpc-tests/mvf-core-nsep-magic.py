a323a8b3c227dd959c086f7198a1634aad2277bc
ftrader <freetrader@tuta.io>
2016-11-15 21:04:28 +0100

    test split of 4-node network into two separate nets which build their own chains
    this does not actually test yet that the netmagic on these nets is what we expect,
    it only tests that a split has occurred as expected

--- qa/rpc-tests/mvf-core-nsep-magic.py
+++ qa/rpc-tests/mvf-core-nsep-magic.py
@@ -7,10 +7,14 @@
 # Test MVF network separation functionality (TRIG)
 #
 # on node 0 and 1, block height trigger is set to height 100
-# on node 2, block height trigger is set to height 200
+# on node 2 and 3, block height trigger is set to height 999999 (don't trigger during test)
 #
-
-import os
+# blocks are generated on node 0 to trigger the fork.
+# it is checked that nodes 0 and 1 are still peers with each other,
+# but not with nodes 2 and 3, and vice versa.
+#
+# some more blocks are generated on the different nodes to check that
+# two separate chains are being formed.
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import *
@@ -24,19 +28,25 @@ class MVF_NSEP_Netmagic_Test(BitcoinTestFramework):
 
     def start_all_nodes(self):
         self.nodes = []
-        self.is_network_split = False
+        self.is_network_split = False    # not in the beginning it isn't
         self.nodes.append(start_node(0, self.options.tmpdir,
                             ["-forkheight=100", ]))
         self.nodes.append(start_node(1, self.options.tmpdir,
                             ["-forkheight=100", ]))
         self.nodes.append(start_node(2, self.options.tmpdir,
-                            ["-forkheight=200", ]))
+                            ["-forkheight=999999", ]))
+        self.nodes.append(start_node(3, self.options.tmpdir,
+                            ["-forkheight=999999", ]))
 
     def setup_network(self):
         self.start_all_nodes()
+        # connect them all to each other
         connect_nodes(self.nodes[0], 1)
+        connect_nodes(self.nodes[0], 2)
+        connect_nodes(self.nodes[0], 3)
         connect_nodes(self.nodes[1], 2)
-        connect_nodes(self.nodes[2], 0)
+        connect_nodes(self.nodes[1], 3)
+        connect_nodes(self.nodes[2], 3)
 
     def prior_fork_detected_on_node(self, node=0):
         """ check in log file if prior fork has been detected and return true/false """
@@ -58,35 +68,79 @@ class MVF_NSEP_Netmagic_Test(BitcoinTestFramework):
         print "Generating 99 pre-fork blocks"
         for b in range(99):
             self.nodes[0].generate(1)
-            self.sync_all()
+        self.sync_all()
+        print "synching blocks"
+        sync_blocks(self.nodes[:4])
+        print "synching mempools"
+        sync_mempools(self.nodes[:4])
+        print "checking that fork did not trigger on any node"
         for n in xrange(len(self.nodes)):
             assert_equal(False, self.is_fork_triggered_on_node(n)
                                 or self.prior_fork_detected_on_node(n))
         print "Fork did not trigger prematurely"
 
-        # check that fork triggers for node 0 at height 100 but not on node 1
+        print "checking block height = 99 on all nodes"
+        assert_equal(99, self.nodes[0].getblockcount())
+        assert_equal(99, self.nodes[1].getblockcount())
+        assert_equal(99, self.nodes[2].getblockcount())
+        assert_equal(99, self.nodes[3].getblockcount())
+
+        print "generating fork block on node 0"
         self.nodes[0].generate(1)
-        self.sync_all()
+        print "synching blocks on nodes 0+1"
+        sync_blocks(self.nodes[:2])
+        print "synching mempools on nodes 0+1"
+        sync_mempools(self.nodes[:2])
+        print "synching blocks on nodes 2+3"
+        sync_blocks(self.nodes[2:])
+        print "synching mempools on nodes 2+3"
+        sync_mempools(self.nodes[2:])
+
         assert_equal(100, self.nodes[0].getblockcount())
         assert_equal(100, self.nodes[1].getblockcount())
-        assert_equal(100, self.nodes[2].getblockcount())
-        assert_equal(True,  self.is_fork_triggered_on_node(0))
-        assert_equal(True,  self.is_fork_triggered_on_node(0))
-        assert_equal(False, self.prior_fork_detected_on_node(0))
-        assert_equal(True, self.is_fork_triggered_on_node(1))
-        assert_equal(False, self.prior_fork_detected_on_node(1))
-        assert_equal(False, self.is_fork_triggered_on_node(2))
-        assert_equal(False, self.prior_fork_detected_on_node(2))
+        assert_equal( 99, self.nodes[2].getblockcount())
+        assert_equal( 99, self.nodes[3].getblockcount())
+        # check nodes 0, 1 have forked
+        for n in (0,1):
+           assert_equal(True,  self.is_fork_triggered_on_node(n))
+           assert_equal(False, self.prior_fork_detected_on_node(n))
+        # ... and nodes 2, 3 have not
+        for n in (2,3):
+            assert_equal(False, self.is_fork_triggered_on_node(n))
+            assert_equal(False, self.prior_fork_detected_on_node(n))
         print "Fork triggered successfully on nodes 0+1 (block height 100)"
+        print "Fork NOT triggered on nodes 2+3 (block height 99)"
 
-        # check nodes 0+1 no longer accept from node 2
-        self.nodes[2].generate(100) 
-        time.sleep(5)
-        for n in xrange(len(self.nodes)):
-            print "block count on node %s: %s" % (n, self.nodes[n].getblockcount())
-        assert_equal(100, self.nodes[0].getblockcount())
-        assert_equal(100, self.nodes[1].getblockcount())
+        # check that the chains are now separate
+        print "generating another post-fork block on node 0"
+        self.nodes[0].generate(1)
+        print "synching blocks on nodes 0+1"
+        sync_blocks(self.nodes[:2])
+        print "synching mempools on nodes 0+1"
+        sync_mempools(self.nodes[:2])
+        print "synching blocks on nodes 2+3"
+        sync_blocks(self.nodes[2:])
+        print "synching mempools on nodes 2+3"
+        sync_mempools(self.nodes[2:])
+        assert_equal(101, self.nodes[0].getblockcount())
+        assert_equal(101, self.nodes[1].getblockcount())
+        assert_equal( 99, self.nodes[2].getblockcount())
+        assert_equal( 99, self.nodes[3].getblockcount())
 
+        print "generating 3 blocks on node 2"
+        self.nodes[2].generate(3)
+        print "synching blocks on nodes 0+1"
+        sync_blocks(self.nodes[:2])
+        print "synching mempools on nodes 0+1"
+        sync_mempools(self.nodes[:2])
+        print "synching blocks on nodes 2+3"
+        sync_blocks(self.nodes[2:])
+        print "synching mempools on nodes 2+3"
+        sync_mempools(self.nodes[2:])
+        assert_equal(101, self.nodes[0].getblockcount())
+        assert_equal(101, self.nodes[1].getblockcount())
+        assert_equal(102, self.nodes[2].getblockcount())
+        assert_equal(102, self.nodes[3].getblockcount())
 
 if __name__ == '__main__':
     MVF_NSEP_Netmagic_Test().main()
